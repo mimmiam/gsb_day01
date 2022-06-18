595210c5eb6f7cf83fc4f411f74befdd90839896
buildsmile <you@example.com>
2022-06-18 10:32:58 +0700

    counter

--- lib/counter.dart
+++ lib/counter.dart
@@ -8,8 +8,31 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter'),
+      ),
+      body: Center(
+        child: Text(
+          'You pressed button $count time',
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          count++;
+          print(count);
+          setState(() {});
+        },
+      ),
+    );
   }
 }
